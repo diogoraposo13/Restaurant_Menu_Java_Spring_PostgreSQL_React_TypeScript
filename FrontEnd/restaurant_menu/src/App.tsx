1bd02db38f0e12c5bfb410f32e3b2307a3f950e2
diogoraposo13 <diogo.raposo@live.com.pt>
2023-08-28 16:12:05 +0100

    Get food data from backend

--- FrontEnd/restaurant_menu/src/App.tsx
+++ FrontEnd/restaurant_menu/src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
+import { FoodData } from './interface/FoodData';
+import { Card } from './components/card/card';
+import { useFoodData } from './hooks/useFoodData';
+import {useState} from 'react'
 
 function App() {
-
+  const {data} = useFoodData();
 
   return (
       <div className='container'>
-        
+        <h1>Cardápio</h1>
+        <div className="card-grid">
+          {data?.map(foodData =>
+              <Card 
+              price={foodData.price}
+              title={foodData.title}
+              image={foodData.image}
+              />
+          )}
+        </div>
       </div>
   )
 }
