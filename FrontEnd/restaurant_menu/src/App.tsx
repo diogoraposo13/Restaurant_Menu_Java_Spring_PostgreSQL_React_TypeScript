6a42f849e1587207963b4206642c346addf4f58d
diogoraposo13 <diogo.raposo@live.com.pt>
2023-08-28 17:01:01 +0100

    FrontEnd - Create new food

--- FrontEnd/restaurant_menu/src/App.tsx
+++ FrontEnd/restaurant_menu/src/App.tsx
@@ -3,9 +3,15 @@ import { FoodData } from './interface/FoodData';
 import { Card } from './components/card/card';
 import { useFoodData } from './hooks/useFoodData';
 import {useState} from 'react'
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
   const {data} = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
       <div className='container'>
@@ -17,8 +23,11 @@ function App() {
               title={foodData.title}
               image={foodData.image}
               />
-          )}
+          )}         
+
         </div>
+        {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+        <button onClick={handleOpenModal}>Novo</button>
       </div>
   )
 }
